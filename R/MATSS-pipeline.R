e96b41636658aaa2c59adc74ced62fc6512d2c6b
DAPPERstats <dapperstats@gmail.com>
2018-09-21 11:15:10 -0700

    cleaning code for building a package
    created base documentation for the package including the description file and initial function documentation
    
    added DESCRIPTION and NAMESPACE files
    
    the scripts that were in the R and data_scripts subdirectories have been put into the scripts_to_pull_in directory to start from a clean package
    
    added a general pacakge documentation script (MATSS-pipeline.R)
    
    included gitignore and Rbuildignore files

---
+++ R/MATSS-pipeline.R
@@ -0,0 +1,11 @@
+#' @title Macroecological Analayses of Time Series Structure pipeline
+#'
+#' @description Analyze the structure of ecological time series.
+#'
+#' @name MATSSpipeline
+#'
+#' @docType package
+#'
+#' @keywords package
+#'
+NULL
