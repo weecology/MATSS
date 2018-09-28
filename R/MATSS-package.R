d15551fbae0cb2f3628835d2462eac3f2deada78
Hao Ye <lhopitalified@gmail.com>
2018-09-28 13:49:27 -0400

    synchronize names and documentation

---
+++ R/MATSS-package.R
@@ -0,0 +1,12 @@
+#' @title Macroecological Analayses of Time Series Structure
+#'
+#' @description Functions for obtaining and processing ecological time series 
+#'   datasets, and for running different analyses.
+#' 
+#' @name MATSS
+#'
+#' @docType package
+#'
+#' @keywords package
+#'
+NULL
