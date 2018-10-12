8e122593da518b93a8296174101ede257c23ca3b
Renata Diaz <diaz.renatam@gmail.com>
2018-10-12 13:41:30 -0400

    add lda report (rmd)

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -7,7 +7,11 @@ lda_analysis <- drake::drake_plan(
     portal_data = portal_data_raw %>%
         select(-period, -censusdate), 
     portal_lda = run_LDA(portal_data, 
-                         max_topics = 6, ncores = 4, nseeds = 20)
+                         max_topics = 6, ncores = 4, nseeds = 20),
+    lda_report = knitr::knit(knitr_in('lda_report.Rmd'), file_out('lda_report.md'), quiet = T)
 )
+
+source('R/analysis-function_LDA.R')
+source('R/data-function_portal.R')
 
 drake::make(lda_analysis)
---
+++ lda_report.md
@@ -0,0 +1,15 @@
+---
+title: "LDA report"
+author: "Renata Diaz"
+date: "10/12/2018"
+output: html_document
+---
+
+
+
+
+```r
+plot_lda_edited(drake::readd(portal_lda), drake::readd(portal_data_raw)$censusdate, select_samples = NULL)
+```
+
+![plot of chunk plot LDA](figure/plot LDA-1.png)
