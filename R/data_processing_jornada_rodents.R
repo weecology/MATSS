106567756b86920902118a3d1c8bb54968c09242
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2018-10-19 13:23:15 -0400

    actual script...

--- R/data_processing_jornada_rodents.R
+++ R/data_processing_jornada_rodents.R
@@ -1,6 +1,6 @@
-library(tidyverse)
+#' @importFrom magrittr "%>%"
 
-process_jornada_data <- function(data_path = "data/Jornada/", 
+process_jornada_data <- function(data_path = "data/", 
                                  rodent_file = file.path(data_path, "jornada_rodents.csv"), 
                                  abundance_file = file.path(data_path, "jornada_abundance_table.RDS"),
                                  covariates_file = file.path(data_path, "jornada_covariates_time.RDS"))
@@ -10,18 +10,18 @@ process_jornada_data <- function(data_path = "data/Jornada/",
     
     # select key columns 
     # filter out unknown species and recaptures
-    jornada_rodents <- select(jornada, year, season, spp, recap) %>% 
-        filter(recap != "Y", spp != "DIPO1", spp != "PERO1", spp != "NA", spp != ".")
+    jornada_rodents <- dplyr::select(jornada, year, season, spp, recap) %>% 
+        dplyr::filter(recap != "Y", spp != "DIPO1", spp != "PERO1", spp != "NA", spp != ".")
     
     # get data into wide format
     # summarize counts for each species in each period
     jornada_abundances <- jornada_rodents %>%
-        group_by(year, season, spp) %>%
-        summarize(count = n())
+        dplyr::group_by(year, season, spp) %>%
+        dplyr::summarize(count = n())
     
     # put data in wide format
     jornada_abundance_table <- jornada_abundances %>%
-        spread(spp, count, fill = 0)
+        dplyr::spread(spp, count, fill = 0)
     
     # split into two dataframes and save
     jornada_covariates_time <- jornada_abundance_table[,1:2]
