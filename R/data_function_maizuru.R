3acef015efcb7214391766fd82ae55773254015f
DAPPERstats <dapperstats@gmail.com>
2019-02-14 10:43:43 -0800

    Update data_function_maizuru.R
    to point to the package file

--- R/data_function_maizuru.R
+++ R/data_function_maizuru.R
@@ -2,20 +2,24 @@
 #'
 #' Import maizuru data from data files
 #' 
-#' @return list of two dataframes (one with abundance data, the other with covariate data) 
-#'   and one list of metadata.
+#' @return list of two dataframes (one with abundance data, the other with 
+#'   covariate data) and one list of metadata.
 #'
 #' @export
 #'
 get_maizuru_data <- function()
 {
-  x <- read.csv(here::here("data", "Maizuru_dominant_sp.csv"))
-  x$Date <- dplyr::select(x, Y, M, D) %>%
-            apply(1, paste, collapse = "-") %>%
-            as.Date()
-  list(abundance = dplyr::select(x, -date_tag, -surf.t, -bot.t, -Y, -M, -D, -Date) %>%
-                            mutate_all(~round(. + 1e-10)), 
-                        covariates = dplyr::select(x, date_tag, surf.t, bot.t, Y, M, D, Date),
-                        metadata = list(timename = "Date", effort = NULL))
+  data_path <- system.file("extdata", "Maizuru_dominant_sp.csv",
+                             package = "MATSS", mustWork = TRUE)
+  raw_data <- read.csv(data_path)
+  raw_data$Date <- dplyr::select(raw_data, Y, M, D) %>%
+                   apply(1, paste, collapse = "-") %>%
+                   as.Date()
+  list(abundance = dplyr::select(raw_data, -date_tag, -surf.t, -bot.t, -Y, -M,
+                                 -D, -Date) %>%
+                    mutate_all(~round(. + 1e-10)), 
+       covariates = dplyr::select(raw_data, date_tag, surf.t, bot.t, Y, M, D, 
+                                  Date),
+       metadata = list(timename = "Date", effort = NULL))
 }
 
