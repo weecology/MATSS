811e49cda5edd0bb7cd8f7230c0a3f8ff535ca0a
Hao Ye <lhopitalified@gmail.com>
2019-02-21 13:58:26 -0500

    include an option to include downloadable data in `build_datasets_plan()`

--- R/build_plans.R
+++ R/build_plans.R
@@ -63,21 +63,39 @@ build_analyses_plan <- function(methods, datasets, ...)
 #' @title Generate a Drake Plan for Datasets
 #' 
 #' @param data_path where to get the downloaded retriever datasets
+#' @param include_downloaded_data whether to also include downloadable datasets
 #' 
 #' @return a drake plan (i.e. a tibble) specifying the targets and commands 
 #'   for gathering datasets
 #' 
 #' @export
 #' 
-build_datasets_plan <- function(data_path = get_default_data_path())
+build_datasets_plan <- function(data_path = get_default_data_path(), 
+                                include_downloaded_data = FALSE)
 {
-    drake::drake_plan(
-        portal_data = get_portal_rodents(),
+    datasets <- drake::drake_plan(
         maizuru_data = get_maizuru_data(),
         jornada_data = get_jornada_data(),
         sgs_data = get_sgs_data(),
-        bbs_data = get_bbs_data(region = 7, path = !!data_path),
-        sdl_data = get_sdl_data(path = !!data_path),
-        mtquad_data = get_mtquad_data(path = !!data_path)
     )
+    if(include_downloaded_data)
+    {
+        datasets <- dplyr::bind_rows(datasets, 
+                                     drake::drake_plan(
+                                         portal_data = get_portal_rodents(),
+                                         bbs_data = get_bbs_data(region = 7, path = !!data_path),
+                                         sdl_data = get_sdl_data(path = !!data_path),
+                                         mtquad_data = get_mtquad_data(path = !!data_path)
+                                     )
+        )
+    }
+    
+    return(datasets)
 }
+    
+    
+    
+    
+    
+    
+    
