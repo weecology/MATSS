46f7b12119d1ef000dd0477f112652062b3714fb
Hao Ye <lhopitalified@gmail.com>
2019-02-25 17:17:01 -0500

    streamline summary stat functions update tests accordingly

--- tests/testthat/test-04-summary-stats.R
+++ tests/testthat/test-04-summary-stats.R
@@ -1,13 +1,37 @@
 context("Time Series Summary Statistics")
 
-test_that("interpolate_obs works", {
-    # input check
-    expect_error(interpolate_obs(mtcars, seq_len(NROW(mtcars))))
-    expect_error(interpolate_obs(matrix(rnorm(16), nrow = 4), 1:10))
-    expect_error(interpolate_obs(sunspot.year, NA))
-    expect_error(interpolate_obs(sunspot.year, mtcars))
-    expect_error(interpolate_obs(sunspot.year, time(sunspot.year), NA))
+test_that("check_obs works", {
+    expect_error(check_obs(NULL))
+    expect_error(check_obs(mtcars))
+    expect_error(check_obs(matrix(rnorm(16), nrow = 4)))
+    expect_error(check_obs(matrix(rnorm(16), nrow = 4), single_dim_obs = FALSE), NA)
+    expect_error(check_obs(c(NA, rnorm(16))), NA)
+})
+
+test_that("check_times works", {
+    expect_error(check_times(NULL))
+    expect_error(check_times(mtcars))
+    expect_error(check_times(time(sunspot.year)), NA)
+    expect_error(check_times(c(NA, time(sunspot.year))), NA)
+    expect_error(check_times(rnorm(16)), NA)
+    expect_error(check_times(c(NA, rnorm(16))), NA)
+})
+
+
+test_that("temp_autocor works", {
+    # output check
+    ts <- sunspot.year
+    ts[c(1, 5, 10:14)] <- NA
+    expect_error(output <- temp_autocor(ts, time(sunspot.year)), NA)
+    expect_identical(length(output), length(ts))
+    expect_false(any(is.na(output)))
     
+    # regression check
+    expect_identical(digest::digest(output), 
+                     "0bd2cccc8efd598126314105fc5552ae")
+})
+
+test_that("interpolate_obs works", {
     # output check
     ts <- sunspot.year
     ts[c(1, 5, 10:14)] <- NA
