0b6730f794eee4e98c020256541c126851cdcc52
Hao Ye <lhopitalified@gmail.com>
2019-02-26 01:13:21 -0500

    misc fixes to tests and travis

--- tests/testthat/test-03-retriever-data.R
+++ tests/testthat/test-03-retriever-data.R
@@ -10,18 +10,27 @@ test_that("data_path functions work", {
     expect_false(check_default_data_path())
 })
 
-test_that("retriever download and importing work", {
+# check if `retriever` is installed
+skip_if_no_retriever <- function() {
+    have_retriever <- reticulate::py_module_available("retriever")
+    if (!have_retriever)
+        skip("retriever not available for testing")
+}
+
+test_that("retriever downloading and importing work", {
+    skip_if_no_retriever()
     Sys.setenv(MATSS_DATA_PATH = test_path)
     expect_equal(get_default_data_path(), test_path)
     
     expect_error(install_retriever_data("veg-plots-sdl"), NA)
     expect_error(dat <- import_retriever_data("veg-plots-sdl"), NA)
-    
-    expect_known_hash(dat$veg_plots_sdl_Count1906, "daf4dd1faa")
-    expect_known_hash(dat$veg_plots_sdl_Photo_info, "a6cb447edc")
-    expect_known_hash(dat$veg_plots_sdl_Plot_corners, "5174728afa")
-    expect_known_hash(dat$veg_plots_sdl_Plots, "3edffbf85a")
-    expect_known_hash(dat$veg_plots_sdl_Seedling_counts, "424b7bbfc3")
-    expect_known_hash(dat$veg_plots_sdl_SMCover, "911b8ab006")
-    expect_known_hash(dat$veg_plots_sdl_SMDensity, "4baa4ce118")
+    expect_known_hash(dat$veg_plots_sdl_Count1906, "4f6b34f60a")
+    expect_known_hash(dat$veg_plots_sdl_Photo_info, "5fa488da2a")
+    expect_known_hash(dat$veg_plots_sdl_Plot_corners, "53e7e72ee9")
+    expect_known_hash(dat$veg_plots_sdl_Plots, "b0c6ec4b6e")
+    expect_known_hash(dat$veg_plots_sdl_Seedling_counts, "dd0d14c55a")
+    expect_known_hash(dat$veg_plots_sdl_SMCover, "6b03c46cbf")
+    expect_known_hash(dat$veg_plots_sdl_SMDensity, "1a5673f8ff")
+    expect_known_hash(dat$veg_plots_sdl_Stake_info, "6e7608632d")
+    expect_known_hash(dat$veg_plots_sdl_Species, "e4276e9361")
 })
