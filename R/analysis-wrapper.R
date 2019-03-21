babb4cfcf819a0818ec1f9d19af1b47a2a4ffceb
Hao Ye <lhopitalified@gmail.com>
2019-03-21 16:50:25 -0400

    update docs and pkgdown config

--- R/analysis-wrapper.R
+++ R/analysis-wrapper.R
@@ -1,17 +1,19 @@
 #' @title Create a function that replicates an analysis for all time series in a
 #'   dataset
 #' 
-#' @description This wrapper takes as input, an analysis function intended to be
-#'   applied to a single time series, and creates a function that will apply 
-#'   that analysis to all the time series as part of a dataset, returning a 
-#'   combined object with all of the results.
-#' 
+#' @description This wrapper takes as input, some analysis function, `fun`, 
+#'   along with additional optional arguments, and returns a function that will 
+#'   run that analysis function on all the time series in a dataset. The return 
+#'   object from that function combined all the relevant output, and also 
+#'   information on the call, for post-processing that is suitable by itself, or 
+#'   as part of a Drake plan.
+#'   
 #' @param fun the analysis function
 #' @param ... extra params to be passed to the analysis function
 #' 
 #' @return a function that takes in a single argument, `dataset`, and returns a 
 #'   tibble with these columns:
-#'   \tabular{ll}{
+#'   \tabular{ ll}{
 #'     \code{results} \tab a combined results data.frame\cr
 #'     \code{metadata} \tab the metadata component of the original dataset\cr
 #'     \code{dataset} \tab the name of the dataset\cr
