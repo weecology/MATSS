10c7d89d3090eef7a2db9bbc979869aef9f8a66a
Hao Ye <lhopitalified@gmail.com>
2019-04-27 11:34:11 -0400

    fix tests to account for NA

--- tests/testthat/test-03-retriever-data.R
+++ tests/testthat/test-03-retriever-data.R
@@ -25,7 +25,9 @@ test_that("retriever downloading and importing work", {
     expect_error(install_retriever_data("veg-plots-sdl"), NA)
     expect_error(dat <- import_retriever_data("veg-plots-sdl"), NA)
     expect_known_hash(dat$veg_plots_sdl_Count1906, "4f6b34f60a")
-    expect_known_hash(dat$veg_plots_sdl_Photo_info, "5fa488da2a")
+    expect_known_hash(is.na(dat$veg_plots_sdl_Photo_info), "fa7b429803")
+    dat$veg_plots_sdl_Photo_info[is.na(dat$veg_plots_sdl_Photo_info)] <- -999999
+    expect_known_hash(dat$veg_plots_sdl_Photo_info, "23666fa87a")
     expect_known_hash(dat$veg_plots_sdl_Plot_corners, "53e7e72ee9")
     expect_known_hash(dat$veg_plots_sdl_Plots, "b0c6ec4b6e")
     expect_known_hash(dat$veg_plots_sdl_Seedling_counts, "dd0d14c55a")
