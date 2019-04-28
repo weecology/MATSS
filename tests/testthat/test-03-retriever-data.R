1b0eb2e2f9e2b413667ad5f41c6e7d72aae937cd
Hao Ye <lhopitalified@gmail.com>
2019-04-28 00:37:02 -0400

    add test for sdl data import

--- tests/testthat/test-03-retriever-data.R
+++ tests/testthat/test-03-retriever-data.R
@@ -1,13 +1,14 @@
 context("Check Retriever interface functions")
 
 test_path <- tempdir()
+
 test_that("data_path functions work", {
+    Sys.unsetenv("MATSS_DATA_PATH")
+    expect_false(check_default_data_path())
+
     Sys.setenv(MATSS_DATA_PATH = test_path)
     expect_equal(get_default_data_path(), test_path)
     expect_true(check_default_data_path())
-    
-    Sys.unsetenv("MATSS_DATA_PATH")
-    expect_false(check_default_data_path())
 })
 
 # check if `retriever` is installed
@@ -19,9 +20,6 @@ skip_if_no_retriever <- function() {
 
 test_that("retriever installing does error checking", {
     skip_if_no_retriever()
-    Sys.setenv(MATSS_DATA_PATH = test_path)
-    expect_equal(get_default_data_path(), test_path)
-    
     expect_error(install_retriever_data("veg-plots-sdl"), NA)
     m <- capture_messages(install_retriever_data("veg-plots-sdl"))
     expect_match(m, "A folder already exists for \"veg-plots-sdl\"")
@@ -29,8 +27,6 @@ test_that("retriever installing does error checking", {
 })
 
 test_that("retriever importing does error checking", {
-    Sys.setenv(MATSS_DATA_PATH = test_path)
-    expect_equal(get_default_data_path(), test_path)
     unused_filename <- tempfile(tmpdir = "")
     unused_filename <- substring(unused_filename, 2, nchar(unused_filename))
     expect_false(file.exists(file.path(test_path, unused_filename)))
@@ -42,9 +38,7 @@ test_that("retriever importing does error checking", {
 })
 
 test_that("retriever importing works", {
-    Sys.setenv(MATSS_DATA_PATH = test_path)
-    expect_equal(get_default_data_path(), test_path)
-    
+    skip_if_no_retriever()
     expect_error(dat <- import_retriever_data("veg-plots-sdl"), NA)
     expect_known_hash(dat$veg_plots_sdl_Count1906, "4f6b34f60a")
 #    expect_known_hash(which(is.na(dat$veg_plots_sdl_Photo_info)), "44cc18c383")
@@ -56,4 +50,11 @@ test_that("retriever importing works", {
     expect_known_hash(dat$veg_plots_sdl_SMDensity, "1a5673f8ff")
     expect_known_hash(dat$veg_plots_sdl_Stake_info, "6e7608632d")
     expect_known_hash(dat$veg_plots_sdl_Species, "e4276e9361")
+})
+
+test_that("get_sdl_data formats data correctly", {
+    skip_if_no_retriever()
+    expect_error(dat <- get_sdl_data(), NA)
+    expect_true(check_data_format(dat))
+    expect_known_hash(dat, "e61d747927")
 })
