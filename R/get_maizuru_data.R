4ca44baa11903f0b364912edf5e15d5792620306
Hao Ye <lhopitalified@gmail.com>
2019-04-29 15:26:55 -0400

    add documentation of Maizuru data closes #60

--- R/get_maizuru_data.R
+++ R/get_maizuru_data.R
@@ -1,3 +1,16 @@
+#' Dataset Maizuru Fish Community
+#' 
+#' A dataset containing the twice-monthly visual census data of the Maizuru fish 
+#'   community (only the 15 species that had total observation count > 1000).
+#' Data are available from https://zenodo.org/record/1181937
+#' 
+#' @name maizuru_data
+#' 
+#' @section Maizuru_dominant_sp.csv
+#'
+#' This data is imported using \code{\link{get_maizuru_data}}
+NULL
+
 #' @title Read in the maizuru community data from a csv file
 #'
 #' Import maizuru data from data files
@@ -6,7 +19,6 @@
 #'   covariate data) and one list of metadata.
 #'
 #' @export
-#'
 get_maizuru_data <- function()
 {
     data_path <- system.file("extdata", "Maizuru_dominant_sp.csv",
