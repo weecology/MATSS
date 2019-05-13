635275aaca1562e8d0357997f9116daac76e7234
Renata Diaz <diaz.renatam@gmail.com>
2019-05-13 11:34:13 -0400

    Remove test for get_bbs_data (removed)

--- tests/testthat/test-04-retriever-data.R
+++ tests/testthat/test-04-retriever-data.R
@@ -1,18 +1,18 @@
 context("Check Retriever datasets")
-
-test_that("get_bbs_data formats data correctly", {
-    data_path <- system.file("extdata", "subsampled",
-                             package = "MATSS", mustWork = TRUE)
-    Sys.setenv(MATSS_DATA_PATH = data_path)
-    expect_error(dat <- get_bbs_data(region = 4, min_num_yrs = 1), NA)
-    expect_true(check_data_format(dat))
-    expect_known_hash(dat$abundance, "0d083ef430")
-    expect_known_hash(which(is.na(dat$covariates)), "40f5faaa59")
-    dat$covariates[is.na(dat$covariates)] <- -999999
-    expect_known_hash(na.omit(dat$covariates), "6c2f809118")
-    expect_known_hash(dat$metadata, "b5adce4593")
-    expect_known_hash(dat, "72aa13aef2")
-})
+# 
+# test_that("get_bbs_data formats data correctly", {
+#     data_path <- system.file("extdata", "subsampled",
+#                              package = "MATSS", mustWork = TRUE)
+#     Sys.setenv(MATSS_DATA_PATH = data_path)
+#     expect_error(dat <- get_bbs_data(region = 4, min_num_yrs = 1), NA)
+#     expect_true(check_data_format(dat))
+#     expect_known_hash(dat$abundance, "0d083ef430")
+#     expect_known_hash(which(is.na(dat$covariates)), "40f5faaa59")
+#     dat$covariates[is.na(dat$covariates)] <- -999999
+#     expect_known_hash(na.omit(dat$covariates), "6c2f809118")
+#     expect_known_hash(dat$metadata, "b5adce4593")
+#     expect_known_hash(dat, "72aa13aef2")
+# })
 
 test_that("get_mtquad_data formats data correctly", {
     data_path <- system.file("extdata", "subsampled",
