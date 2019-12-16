f58ff9257a9d579fad8b31ac29cd0be342f1e254
Hao Ye <lhopitalified@gmail.com>
2019-12-16 17:44:10 -0500

    add test for compendium creation

---
+++ tests/testthat/test-10-compendium-creation.R
@@ -0,0 +1,20 @@
+context("MATSS research compendium")
+
+test_path <- file.path(tempdir(), "test")
+
+test_that("creating a compendium works", {
+    expect_error(create_MATSS_compendium(test_path), NA)
+})
+
+test_that("compendium files exist", {
+    expect_true(file.exists(test_path))
+    expect_true(file.exists(file.path(test_path, "DESCRIPTION")))
+    expect_true(file.exists(file.path(test_path, "NAMESPACE")))
+    expect_true(file.exists(file.path(test_path, ".gitignore")))
+    expect_true(file.exists(file.path(test_path, ".Rbuildignore")))
+    expect_true(file.exists(file.path(test_path, "test.Rproj")))
+    expect_true(file.exists(file.path(test_path, "analysis")))
+    expect_true(file.exists(file.path(test_path, "analysis", "pipeline.R")))
+    expect_true(file.exists(file.path(test_path, "analysis", "report.Rmd")))
+    expect_true(file.exists(file.path(test_path, "R")))
+})
