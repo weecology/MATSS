c36a209f8beb93f69d27e507b0e07b744b7d3d17
Hao Ye <lhopitalified@gmail.com>
2020-03-05 22:50:49 -0500

    check species table encoding

--- tests/testthat/test-04-retriever-data.R
+++ tests/testthat/test-04-retriever-data.R
@@ -11,5 +11,7 @@ test_that("veg-plots-sdl data retrieval works correctly", {
     
     expect_error(dat <- get_sdl_data(), NA)
     expect_known_hash(dat$metadata$species_table, "1ceab10626")
-    expect_dataset(dat, "b076948e1b", "de2dc7f655", "96918f0dc5")
+    expect_known_hash(dat$metadata$species_table$reportedname, "2688f9de54")
+    expect_known_hash(dat$metadata$species_table$species_name, "42c0957e37")
+    MATSS:::expect_dataset(dat, "b076948e1b", "de2dc7f655", "96918f0dc5")
 })
