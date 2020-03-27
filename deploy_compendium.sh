728f5bce375deb55cbf8cf8fc80877ff5b9f55e4
Hao Ye <lhopitalified@gmail.com>
2020-03-27 17:13:02 -0400

    working build and deploy to MATSSdemo repo

--- deploy_compendium.sh
+++ deploy_compendium.sh
@@ -1,4 +1,27 @@
+# setup vars
+GIT_USER="Weecology Deploy Bot"
+GIT_EMAIL="weecologydeploy@weecology.org"
+REPO="MATSSdemo"
+GH_REPO="github.com/weecology/$REPO.git"
+LAST_COMMIT_MESSAGE=$(git log --format=%B -n 1)
+
+# build compendium
 Rscript -e 'MATSS::create_MATSS_compendium("../MATSSdemo", "Travis CI")'
-Rscript -e 'devtools::install("../MATSSdemo")'
-Rscript -e 'source("../MATSSdemo/analysis/pipeline.R")'
+cd ../MATSSdemo
+Rscript -e 'devtools::document(".")'
+Rscript -e 'devtools::install(".", upgrade = "never")'
+Rscript -e 'source("analysis/pipeline.R")'
 echo 'Successfully built compendium!'
+
+# add newest compendium to the compendium repo
+mkdir ../scratch
+cd ../scratch
+git clone git://${GH_REPO}
+cp -r ../MATSSdemo/* ${REPO}
+cd ${REPO}
+git remote
+git config user.email ${EMAIL}
+git config user.name ${USER}
+git add .
+git commit -m "Update Compendium: Travis Build $TRAVIS_BUILD_NUMBER" -m "$LAST_COMMIT_MESSAGE"
+git push "https://${COMPENDIUM_DEPLOY_TOKEN}@${GH_REPO}" master > /dev/null 2>&1
