58935ab47e91c97f03b5ef83e20b46e7bf76f388
Hao Ye <lhopitalified@gmail.com>
2020-04-28 12:11:47 -0400

    fix hash (refactor orders species alphabetically) version bump

--- tests/testthat/test-04-retriever-data.R
+++ tests/testthat/test-04-retriever-data.R
@@ -10,6 +10,5 @@ test_that("veg-plots-sdl data retrieval works correctly", {
     expect_false(is.null(dat))
     
     expect_error(dat <- get_sdl_data(), NA)
-#    expect_known_hash(unlist(dat$metadata$species_table), "d98472ffcc")
-    expect_dataset(dat, "b076948e1b", "de2dc7f655", "d52a190865", "d98472ffcc")
+    expect_dataset(dat, "ff06f7af67", "de2dc7f655", "d52a190865", "d98472ffcc")
 })
